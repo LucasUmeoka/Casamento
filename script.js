fe9899ddc4fbeb54dbaa3a462ad6f0f377edd6f9
Lucas Umeoka <88043583+LucasUmeoka@users.noreply.github.com>
2022-12-13 01:30:21 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+window.sr = ScrollReveal({ reset: true});
+
+sr.reveal('.foto__1', {duration: 1000});
+sr.reveal('.foto__2', {duration: 1000});
+sr.reveal('.foto__3', {duration: 1000});
+sr.reveal('.foto__4', {duration: 1000});
+sr.reveal('.foto__5', {duration: 1000});
+sr.reveal('.foto__6', {duration: 1000});
+sr.reveal('.foto__7', {duration: 1000});
+sr.reveal('.foto__8', {duration: 1000});
+sr.reveal('.foto__9', {duration: 1000});
